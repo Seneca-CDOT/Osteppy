d3a06de94479887ac821f661528403b4586b1e3e
Naiuhz <naiuhz@gmail.com>
2019-12-10 23:56:28 -0500

    Added viewEODReminder method

--- tests/testEODList.js
+++ tests/testEODList.js
@@ -2,18 +2,57 @@
 
 const path = require("path");
 const fs = require("fs");
+const EODReminderPath = path.join(__dirname, "../config-files/EODReminderTimes.json");
+
 var EODJSON;
 
-/*
-const EODList = require('./EODList');
 
-const EOD = new EODList();
+var EODContent = fs.readFileSync(EODReminderPath);
 
-//EOD.load().catch(console.log);
-EOD.loadEOD().catch(console.log);*/
+EODJSON = JSON.parse(EODContent);
 
+//console.log(EODJSON);
 
+// Saves an EOD reminder
+var addEODReminder = (name, message) => {
+    var messageArr = message.split(";")
+    var reminderJSON = JSON.parse('{ "time": ' + '"' + messageArr[0] 
+    + '", ' + '"weekday": [' +  messageArr[1].split(",")
+    + '], "message": ' +  messageArr[2] + "}");
+    //console.log(reminderJSON)
+    if (EODJSON[name] !== undefined){
+        //console.log(EODJSON[name]["reminders"]);
+        EODJSON[name]["reminders"].push(reminderJSON)
+    } else {
+        console.log(name + " not found.");
+        EODJSON[name] = {"reminders" : [reminderJSON]}
+    }
+    fs.writeFileSync(EODReminderPath, JSON.stringify(EODJSON), 'utf8');
+}
 
+// Returns EOD reminders in JSON format
+var viewEODReminders = (name) => {
+    if (EODJSON[name] !== undefined){
+        return EODJSON[name]["reminders"];
+    } else {
+        return ("Sorry " + name + ", but you have no EOD Reminders.");
+    }
+}
+
+// Removes an EOD reminder
+var removeEODReminder = (name, number) => {
+
+}
+
+//addEODReminder("naiuhz", "17:00;1,2,3,4,5;\"It's 5PM, remember to submit EOD! :ayaya:\"");
+//addEODReminder("Jason", "10:00;1,2,3,4,5;\"It's 10AM, remember to submit yesterday's EOD! :ayaya:\"");
+
+console.log(viewEODReminders("naiuhz"));
+
+//console.log(EODJSON["naiuhz"]["reminders"]);
+//console.log(EODJSON["Jason"]["reminders"]);
+
+//console.log()
 
 //There was an attempt to use the async function: readFile 
 /*let readEOD = async function() {
@@ -46,36 +85,3 @@ let load = async () => {
     }
 }
 load();*/
-
-var EODContent = fs.readFileSync(path.join(__dirname, "../config-files/EODReminderTimes.json"));
-
-EODJSON = JSON.parse(EODContent);
-
-
-//console.log(EODJSON);
-
-
-var addEODReminder = (name, message) => {
-    var messageArr = message.split(";")
-    var reminderJSON = JSON.parse('{ "time": ' + '"' + messageArr[0] 
-    + '", ' + '"weekday": [' +  messageArr[1].split(",")
-    + '], "message": ' +  messageArr[2] + "}");
-    //console.log(reminderJSON)
-    if (EODJSON[name] !== undefined){
-        //console.log(EODJSON[name]["reminders"]);
-        EODJSON[name]["reminders"].push(reminderJSON)
-    } else {
-        console.log(name + " not found.");
-        EODJSON[name] = {"reminders" : [JSON.parse('{ "time": ' + '"' + messageArr[0] 
-        + '", ' + '"weekday": [' +  messageArr[1].split(",")
-        + '], "message": ' +  messageArr[2] + "}")]}
-    }
-}
-
-addEODReminder("naiuhz", "17:00;1,2,3,4,5;\"It's 5PM, remember to submit EOD! :ayaya:\"");
-addEODReminder("Jason", "10:00;1,2,3,4,5;\"It's 10AM, remember to submit yesterday's EOD! :ayaya:\"");
-
-console.log(EODJSON["naiuhz"]["reminders"]);
-console.log(EODJSON["Jason"]["reminders"]);
-
-//console.log()
