68d791b388689a70b3f52e9eec5a6c466eb039e9
Naiuhz <naiuhz@gmail.com>
2019-12-16 17:24:01 -0500

    reset_user_list_setup.sh: Added reset user list setup script

---
+++ src/reset_user_list_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Server setup script for SystemD Services
+NEWDIR=$(dirname "$(readlink -fm "$0")")
+sed -i "s@OLDDIR@$NEWDIR@g" $NEWDIR/osteppy-reset-user-list.service
+
+# Copy Unit Files
+
+cp $NEWDIR/osteppy-reset-user-list.service /usr/lib/systemd/system/
+
+# Reload
+systemctl daemon-reload
+
+# Enable Services
+systemctl enable osteppy-reset-user-list
+
+# Start Services
+systemctl start osteppy-reset-user-list
+
+# Check Osteppy Reset User List Service Status
+systemctl status osteppy-reset-user-list
