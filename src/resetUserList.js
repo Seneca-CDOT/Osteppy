475896d1d0f466beed4c16a4a40aa5c43a1a76d4
Naiuhz <naiuhz@gmail.com>
2019-12-18 15:42:13 -0500

    resetUserList.js: Wrote functions for finding next weekday and getting the miliseconds to next weekday

--- src/resetUserList.js
+++ src/resetUserList.js
@@ -10,14 +10,29 @@
  * of the day
  *****************************************************/
 
+const path = require('path');
+const { execSync } = require('child_process');
+
 const cpCommand = 'cp ' + path.join(__dirname, '../config-files/RAs.txt') + " " + path.join(__dirname, '../config-files/sleepyRAs.txt')
+const today = new Date();
+const resetHour = 10;
 
 resetRAList = () => {
     execSync(cpCommand);
 };
 
-const calculateNextWeekday = () => {
-
+const findNextWeekday = () => {
+    // If today is a weekday other than Friday, reset the user list tomorrow at 10AM
+    if (today.getDay() >= 1 &&  today.getDay() <= 4){
+        return calculateNextWeekday(1); 
+    } else if (today.getDay() == 5) { //If today is Friday, reset the user list next Monday
+        return calculateNextWeekday(3);
+    }
 }
 
-setTimeout(() => resetRAList(), calculateNextWeekday());
+const calculateNextWeekday = (nextWeekday) => {
+    const restartDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + nextWeekday, resetHour, 0, 0);
+    return restartDate - today;
+}
+
+setTimeout(() => resetRAList(), findNextWeekday());
