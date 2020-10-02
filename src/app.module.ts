29bd89e38d56b4cb6fd29158e70d387ba8fc7c86
Josue <josue.quilon-barrios@senecacollege.ca>
2020-10-01 21:56:46 -0400

    feat: add @nestjs/config module to use .env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import AppController from './app.controller';
 import AppService from './app.service';
 import UserService from './Users/users.service';
 import { User, UserSchema } from './Users/schemas/user.schema';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/users'), MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://mongodb/users'),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [AppController],
   providers: [AppService, UserService],
 })
