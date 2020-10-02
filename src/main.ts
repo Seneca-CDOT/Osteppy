955b371680ad7d3219e6c52e63720ded0da82833
Josue <josue.quilon-barrios@senecacollege.ca>
2020-10-01 22:01:39 -0400

    refactor: fix format errors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import AppModule from './app.module';
 
 const port = parseInt(process.env.PORT || '3000', 10);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
