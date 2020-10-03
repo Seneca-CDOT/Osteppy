45088b7baaf6d3517833479b0b5933ddd30403f6
Josue <josue.quilon-barrios@senecacollege.ca>
2020-10-03 00:42:30 -0400

    refactor: fix problem destructuring .env var

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import AppModule from './app.module';
 
-const port = parseInt(process.env.PORT || '3000', 10);
+const { PORT } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(port);
+  await app.listen(PORT || 3000);
 }
 bootstrap();
