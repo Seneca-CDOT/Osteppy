b4f9f035e246cfdb91bea43fdd6685d7a35227d2
pynnl <pynnl@outlook.com>
2021-05-22 18:01:21 -0400

    fix: remove unused NestJS ConfigurationModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import AppController from './app.controller';
 import AppService from './app.service';
@@ -9,7 +8,6 @@ import UserModule from './user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
     MongooseModule.forRoot(`mongodb://${MONGO.HOST}`, {
       dbName: 'osteppy',
       user: MONGO.USER,
